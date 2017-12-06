88d53eb5cc77ac47a2c04526df45e07927a616e4
heero7 <kevin7.mudia@gmail.com>
2017-12-06 17:27:58 -0600

    Added artist list to populate in home

---
+++ src/components/artistslist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+// list is received from home
+const ArtistsList = (props) => {
+    console.log(props);
+    return (
+        <div>List</div>
+    )
+}
+
+export default ArtistsList;
--- src/components/home.js
+++ src/components/home.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 
 // Components
 import Banner from '../components/banner';
+import ArtistsList from '../components/artistslist'
+
+//variable that holds the URL
+const URL_ARTISTS = 'http://localhost:3004/artists';
 
 class Home extends Component {
+    // receive data from API 
+    // via Props for state
+    constructor(props){
+        super(props);
+
+        // store everything inside a state
+        this.state = {
+            //key is artists
+            artists: '' // starts as null when first loaded
+        }
+    }
+
     render(){
         return(
-            <Banner></Banner>
+            <div>
+                <Banner></Banner>
+                <ArtistsList></ArtistsList>
+            </div>
         )
     }
 }
